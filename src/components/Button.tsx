92b31c37d39fbf273b4d17e84a1fad024d6a61c2
Zyrenth <work@zyrenth.dev>
2024-12-14 21:28:54 +0100

    added class, color, labelId utils and Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,155 @@
+'use client';
+
+import { cva, VariantProps } from 'class-variance-authority';
+import React, { ButtonHTMLAttributes } from 'react';
+
+import { cn } from '<{utils}>/class';
+
+type Props = VariantProps<typeof btn>;
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, Props {
+    /**
+     * @description Whether the button is disabled.
+     * @default false
+     */
+    disabled?: boolean;
+}
+
+const btn = cva(
+    [
+        'inline-flex items-center justify-center gap-2.5 rounded-md transition-all ease-out duration-[150ms]',
+        'text-center font-medium leading-[normal] overflow-hidden whitespace-nowrap text-ellipsis',
+        'outline-none focus:ring-4 focus:ring-black/20 dark:focus:ring-white/20',
+    ],
+    {
+        variants: {
+            /**
+             * @description The variant of the button.
+             * @default 'primary'
+             */
+            variant: {
+                primary:
+                    'bg-black text-white dark:bg-white dark:text-black hover:enabled:opacity-75 active:enabled:opacity-100',
+                secondary:
+                    'bg-black/10 text-black dark:bg-white/10 dark:text-white border border-black/10 dark:border-white/10 hover:enabled:opacity-75 active:enabled:opacity-100',
+                outline:
+                    'border border-black/15 dark:border-white/15 text-black dark:text-white hover:enabled:bg-black/10 dark:hover:enabled:bg-white/10 hover:enabled:border-black/0 hover:enabled:dark:border-white/0 active:enabled:bg-transparent dark:active:enabled:bg-transparent active:enabled:border-black/15 dark:active:enabled:border-white/15',
+                link: 'text-black dark:text-white underline hover:enabled:opacity-75 active:enabled:opacity-100',
+                blank: 'text-black dark:text-white hover:enabled:bg-black/10 dark:hover:enabled:bg-white/10 active:enabled:bg-transparent dark:active:enabled:bg-transparent',
+                /* <<
+                '{color}': 'bg-{color}-soft-light-bg/30 text-{color}-soft-light-text dark:bg-{color}-soft-dark-bg/20 dark:text-{color}-soft-dark-text hover:enabled:opacity-75 active:enabled:opacity-100 focus:ring-{color}-soft-dark/20 dark:focus:ring-{color}-soft-light/10',
+                >> */
+            },
+            /**
+             * @description The appearance of the button.
+             * @default 'normal'
+             */
+            appearance: {
+                normal: '',
+                glossy: '',
+            },
+            /**
+             * @description The size of the button.
+             * @default 'md'
+             */
+            size: {
+                sm: 'px-3.5 py-2 text-sm',
+                md: 'px-4 py-3 text-sm',
+                lg: 'px-[18px] py-3 text-base',
+                xl: 'px-[18px] py-3.5 text-base',
+            },
+            /**
+             * @description The rounding of the button.
+             * @default 'md'
+             */
+            rounding: {
+                none: 'rounded-none',
+                full: 'rounded-full',
+                sm: 'rounded-sm',
+                md: 'rounded-md',
+                lg: 'rounded-lg',
+                xl: 'rounded-xl',
+            },
+            /**
+             * @description Whether the button is disabled.
+             * @default false
+             */
+            disabled: {
+                true: '!opacity-50 cursor-not-allowed',
+            },
+        },
+        compoundVariants: [
+            {
+                variant: 'primary',
+                appearance: 'glossy',
+                className:
+                    'bg-gradient-to-b from-black/75 to-black/100 dark:from-white/100 dark:to-white/75 !bg-transparent',
+            },
+            {
+                variant: 'secondary',
+                appearance: 'glossy',
+                className:
+                    'bg-gradient-to-b from-black/10 to-black/20 dark:from-white/20 dark:to-white/10 !bg-transparent',
+            },
+            {
+                variant: 'outline',
+                appearance: 'glossy',
+                className:
+                    'bg-gradient-to-b from-black/0 to-black/0 hover:enabled:from-black/10 hover:enabled:to-black/20 hover:enabled:dark:from-white/20 hover:enabled:dark:to-white/10 !bg-transparent',
+            },
+            {
+                variant: 'blank',
+                appearance: 'glossy',
+                className:
+                    'bg-gradient-to-b from-black/0 to-black/0 hover:enabled:from-black/10 hover:enabled:to-black/20 hover:enabled:dark:from-white/20 hover:enabled:dark:to-white/10 !bg-transparent',
+            },
+            /* <<
+            {
+                variant: '{color}',
+                appearance: 'glossy',
+                className:
+                    'bg-gradient-to-b from-{color}-soft-light-bg/20 to-{color}-soft-light-bg/50 dark:from-{color}-soft-dark-bg/20 dark:to-{color}-soft-dark-bg/10 !bg-transparent',
+            },
+            >> */
+        ],
+        defaultVariants: {
+            variant: 'primary',
+            appearance: 'normal',
+            size: 'md',
+            rounding: 'md',
+            disabled: false,
+        },
+    },
+);
+
+/**
+ * @description A button component.
+ * @example <Button variant="primary">Click me</Button>
+ * @example <Button variant="secondary" disabled>Click me</Button>
+ * @example <Button appearance="glossy">Click me</Button>
+ */
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(function Button(
+    { children, className, variant, appearance, size, rounding, disabled, type, ...props },
+    ref,
+) {
+    const role = props.role || 'button';
+    delete props.role;
+
+    const buttonClass = btn({ variant, appearance, size, rounding, disabled });
+
+    return (
+        <button
+            ref={ref}
+            role={role}
+            aria-disabled={disabled}
+            className={cn(buttonClass, className)}
+            disabled={disabled}
+            type={type ?? 'button'}
+            {...props}
+        >
+            {children}
+        </button>
+    );
+});
+
+Button.displayName = 'Button';
+export default Button;
---
+++ src/utils/labelId.ts
@@ -0,0 +1,18 @@
+import { randomBytes } from 'node:crypto';
+
+export default class LabelId {
+    private format = 'quantum:{{id}}:{{label}}';
+    private labels = new Map<string, string>();
+
+    constructor() { }
+
+    public create(label: string): string {
+        const id = this.format.replaceAll('{{id}}', randomBytes(16).toString('hex')).replaceAll('{{label}}', label);
+        this.labels.set(label, id);
+        return id;
+    }
+
+    public get(label: string): string {
+        return this.labels.get(label) || '';
+    }
+}
