e53ea8e235269d0fd3a566b9a86674597012eb6b
Zyrenth <work@zyrenth.dev>
2024-12-18 23:52:29 +0100

    disable type checks during builds temporarily

--- www/next.config.ts
+++ www/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: 'export',
     distDir: '../out',
+    typescript: {
+        ignoreBuildErrors: true,
+    },
 };
 
 export default nextConfig;
